b59e3c6e76cbe6f935f76a1936a0ffe7e3304c78
indrix <indrix@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2006-01-18 02:54:28 +0000

    Initial version

---
+++ core/src/main/java/net/indrix/arara/vo/City.java
@@ -0,0 +1,68 @@
+package net.indrix.arara.vo;
+
+/**
+ * @author Jeff
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class City {
+
+    private int id;
+    private State state;
+    private String name;
+    
+    
+	/**
+	 * @return
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return
+	 */
+	public State getState() {
+		return state;
+	}
+
+	/**
+	 * @param i
+	 */
+	public void setId(int i) {
+		id = i;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setName(String string) {
+		name = string;
+	}
+
+	/**
+	 * @param state
+	 */
+	public void setState(State state) {
+		this.state = state;
+	}
+
+    public String toString(){
+        StringBuffer buffer = new StringBuffer();
+        
+        buffer.append(id);
+        buffer.append(" - ");
+        buffer.append(name);
+        buffer.append(" - ");
+        buffer.append(state.getAcronym());
+        return buffer.toString();
+    }
+}

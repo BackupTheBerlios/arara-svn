793ac10f1c4d329169308703b40a3a8ac99df432
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2006-01-20 23:39:24 +0000

    changed package

--- core/src/main/java/net/indrix/arara/model/exceptions/SoundProcessingException.java
+++ core/src/main/java/net/indrix/arara/model/exceptions/SoundProcessingException.java
@@ -4,7 +4,7 @@
  * To change the template for this generated file go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-package net.indrix.model.exceptions;
+package net.indrix.arara.model.exceptions;
 
 /**
  * @author Jeff

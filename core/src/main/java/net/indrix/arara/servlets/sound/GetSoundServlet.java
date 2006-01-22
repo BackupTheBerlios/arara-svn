5c2bbc72f976c77bd90488e0512eea5e61d08d06
indrix <indrix@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2006-01-22 21:02:42 +0000

    change package name

--- core/src/main/java/net/indrix/arara/servlets/sound/GetSoundServlet.java
+++ core/src/main/java/net/indrix/arara/servlets/sound/GetSoundServlet.java
@@ -4,7 +4,7 @@
  * To change the template for this generated file go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-package net.indrix.servlets.sound;
+package net.indrix.arara.servlets.sound;
 
 import java.io.IOException;
 import java.io.InputStream;
--- core/src/main/java/net/indrix/arara/servlets/user/LoginServlet.java
+++ core/src/main/java/net/indrix/arara/servlets/user/LoginServlet.java
@@ -4,7 +4,7 @@
  * To change the template for this generated file go to
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
-package net.indrix.servlets.user;
+package net.indrix.arara.servlets.user;
 
 import java.io.IOException;
 import java.sql.SQLException;

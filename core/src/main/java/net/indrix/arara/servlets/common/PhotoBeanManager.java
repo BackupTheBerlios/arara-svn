baf1824a539db4b1f8636dec87e0e410cb6da077
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2006-01-25 00:15:20 +0000

    Initial version

---
+++ core/src/main/java/net/indrix/arara/servlets/common/PhotoBeanManager.java
@@ -0,0 +1,38 @@
+/*
+ * Created on 20/01/2006
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package net.indrix.arara.servlets.common;
+
+import java.util.List;
+import java.util.Map;
+
+import net.indrix.arara.bean.UploadPhotoBean;
+import net.indrix.arara.servlets.ServletConstants;
+
+/**
+ * @author Jeff
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class PhotoBeanManager extends BeanManager{
+    /**
+     * This method updates in the bean data related to the photo
+     * @param data
+     * @param bean
+     * @param errors
+     */
+    protected void updateMediaData(Map data, UploadPhotoBean bean, List errors, boolean validate) {
+        logger.debug("Updating photo data...");
+        logger.debug(bean);
+        logger.debug(data);
+        bean.setCamera((String) data.get(ServletConstants.CAMERA));
+        bean.setLens((String) data.get(ServletConstants.LENS));
+        bean.setFilm((String) data.get(ServletConstants.FILM));
+        bean.setDate((String) data.get(ServletConstants.DATE));
+    }
+
+}

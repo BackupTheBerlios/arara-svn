ab970c482aa6929a389a0c8314e330d392fe134c
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2006-10-16 22:23:23 +0000

    added treatment when user is not logged in

--- core/trunk/src/main/java/net/indrix/arara/servlets/photo/InitCommentPhotoServlet.java
+++ core/trunk/src/main/java/net/indrix/arara/servlets/photo/InitCommentPhotoServlet.java
@@ -16,7 +16,9 @@ import javax.servlet.http.HttpSession;
 import net.indrix.arara.dao.DatabaseDownException;
 import net.indrix.arara.model.PhotoModel;
 import net.indrix.arara.servlets.ServletConstants;
+import net.indrix.arara.servlets.ServletUtil;
 import net.indrix.arara.vo.Photo;
+import net.indrix.arara.vo.User;
 
 import org.apache.log4j.Logger;
 
@@ -26,35 +28,53 @@ public class InitCommentPhotoServlet extends HttpServlet {
      */
     static Logger logger = Logger.getLogger("net.indrix.aves");
 
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        doPost(req, res);
+    }
+
     /**
      * 
      */
-    public void doGet(HttpServletRequest req, HttpServletResponse res)
-        throws ServletException, IOException {
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
 
         RequestDispatcher dispatcher = null;
         ServletContext context = this.getServletContext();
         String nextPage = null;
         HttpSession session = req.getSession();
-        
-        PhotoModel model = new PhotoModel();
-        Photo photo = null;
-        int photoId = Integer.parseInt(req.getParameter("photoId"));
-        try {
-            photo = model.retrieve(photoId);
-            session.setAttribute(ServletConstants.CURRENT_PHOTO, photo);
-        } catch (DatabaseDownException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        List<String> errors = new ArrayList<String>();
+
+        User user = (User) session.getAttribute(ServletConstants.USER_KEY);
+        if (user == null) {
+            logger.debug("InitIdentificationPhotoServlet: USER is not logged...");
+            errors.add(ServletConstants.USER_NOT_LOGGED);
+            // put errors in request
+            req.setAttribute(ServletConstants.ERRORS_KEY, errors);
+            nextPage = ServletConstants.LOGIN_PAGE;
+
+            String nextResourceToExecute = ServletUtil.getResource(req);
+            req.setAttribute(ServletConstants.NEXT_RESOURCE_AFTER_LOGIN,
+                    nextResourceToExecute);
+        } else {
+            PhotoModel model = new PhotoModel();
+            Photo photo = null;
+            int photoId = Integer.parseInt(req.getParameter("photoId"));
+            try {
+                photo = model.retrieve(photoId);
+                session.setAttribute(ServletConstants.CURRENT_PHOTO, photo);
+            } catch (DatabaseDownException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            nextPage = ServletConstants.COMMENT_PAGE;
         }
 
-        nextPage = ServletConstants.COMMENT_PAGE;
-      
-        dispatcher = context.getRequestDispatcher(nextPage);
         logger.debug("Dispatching to " + nextPage);
+        dispatcher = context.getRequestDispatcher(nextPage);
         dispatcher.forward(req, res);
 
     }

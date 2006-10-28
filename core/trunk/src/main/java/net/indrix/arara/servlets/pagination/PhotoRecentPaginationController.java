ed8cc19459cceb71fde5f92e9589f9e0b1b708ca
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2006-10-28 17:53:55 +0000

    New class

---
+++ core/trunk/src/main/java/net/indrix/arara/servlets/pagination/PhotoRecentPaginationController.java
@@ -0,0 +1,29 @@
+package net.indrix.arara.servlets.pagination;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import net.indrix.arara.dao.DatabaseDownException;
+
+/**
+ * This class is responsible for retrieving the data for more recent photos
+ * 
+ * @author Jeff
+ *
+ */
+public class PhotoRecentPaginationController extends PhotoPaginationController {
+
+    public PhotoRecentPaginationController(int photosPerPage) {
+        super(photosPerPage, false);
+    }
+
+    /**
+     * @see net.indrix.arara.servlets.pagination.PaginationController#retrieveAllData()
+     */
+    protected List retrieveAllData() throws DatabaseDownException, SQLException {
+        List listOfPhotos = null;
+        listOfPhotos = model.retrievePhotoIDsForRecentPhotos();
+        return listOfPhotos;
+    }
+
+}

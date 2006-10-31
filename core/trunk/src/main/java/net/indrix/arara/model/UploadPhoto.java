becd4cbd950f346a5ddb12d06ae9b78cdbcab401
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2006-10-31 02:16:35 +0000

    Added code to verify new specie

--- core/trunk/src/main/java/net/indrix/arara/model/UploadPhoto.java
+++ core/trunk/src/main/java/net/indrix/arara/model/UploadPhoto.java
@@ -19,17 +19,27 @@ import java.io.InputStream;
 import java.sql.SQLException;
 
 import javax.imageio.ImageIO;
+import javax.mail.internet.AddressException;
 import javax.swing.ImageIcon;
+
+import org.apache.log4j.Logger;
 
 import net.indrix.arara.dao.DatabaseDownException;
 import net.indrix.arara.dao.PhotoDAO;
 import net.indrix.arara.dao.SpecieDAO;
+import net.indrix.arara.model.email.AbstractPhotoEmailSender;
 import net.indrix.arara.model.email.IdentificationPhotoEmailSender;
+import net.indrix.arara.model.email.NewSpecieEmailSender;
 import net.indrix.arara.model.email.PhotoEmailSender;
 import net.indrix.arara.model.exceptions.ImageProcessingException;
+import net.indrix.arara.tools.email.MailClass;
+import net.indrix.arara.tools.email.MailSender;
+import net.indrix.arara.tools.email.NoRecipientException;
+import net.indrix.arara.utils.PropertiesManager;
 import net.indrix.arara.vo.ImageFile;
 import net.indrix.arara.vo.Photo;
 import net.indrix.arara.vo.Specie;
+import net.indrix.arara.vo.Statistics;
 
 /**
  * @author wjs085
@@ -38,207 +48,221 @@ import net.indrix.arara.vo.Specie;
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
 public class UploadPhoto extends AbstractUpload {
-	/**
-	 * The default width value, in case the value from properties could not be
-	 * read
-	 */
-	public static final int W = 240;
+    /**
+     * The default width value, in case the value from properties could not be
+     * read
+     */
+    public static final int W = 240;
 
-	/**
-	 * the width for the thumbnail
-	 */
-	private static final int wForSmallPhoto = setSmallWidth(PhotoUtil
-			.getSmallWidth());
+    /**
+     * the width for the thumbnail
+     */
+    private static final int wForSmallPhoto = setSmallWidth(PhotoUtil.getSmallWidth());
 
-	public void uploadPhoto(Photo photo) throws DatabaseDownException,
-			SQLException, ImageProcessingException {
+    public void uploadPhoto(Photo photo) throws DatabaseDownException,
+            SQLException, ImageProcessingException {
 
-		logger.debug("Calling createSmallPhoto with " + photo);
-		createSmallPhoto(photo);
+        logger.debug("Calling createSmallPhoto with " + photo);
+        createSmallPhoto(photo);
 
-		if (isPhotoValid(photo)) {
-			PhotoDAO dao = new PhotoDAO();
-			logger.debug("Calling insert in DAO object");
-			dao.insert(photo);
+        if (isPhotoValid(photo)) {
+            PhotoDAO dao = new PhotoDAO();
+            logger.debug("Calling insert in DAO object");
 
-			// retrieve data from database, so all data will be loaded
-			SpecieDAO sDao = new SpecieDAO();
-			Specie s = sDao.retrieve(photo.getSpecie().getId());
-			photo.setSpecie(s);
+            boolean newSpecie;
+            if (dao.hasSpecieAPhoto(photo.getSpecie().getId())) {
+                newSpecie = false;
+            } else {
+                newSpecie = true;
+            }
+            logger.debug("New Specie = " + newSpecie);
 
-			// send email to users
-			logger.debug("Sending email");
-			PhotoEmailSender emailSender = new PhotoEmailSender(photo);
-			emailSender.sendEmail();
-		} else {
-			throw new ImageProcessingException(
-					"Real image or thumbnail is wrong" + photo);
-		}
-	}
+            dao.insert(photo);
 
-	/**
-	 * This method verifies if the image was successfully uploaded, and that the
-	 * thumbnail was successfully created
-	 * 
-	 * @param photo
-	 *            The photo to be checked
-	 * 
-	 * @return true if the photo is ok, false otherwise
-	 */
-	private boolean isPhotoValid(Photo photo) {
-		return (photo.getRealImage().getWidth() > 0)
-				&& (photo.getRealImage().getHeight() > 0)
-				&& (photo.getSmallImage().getWidth() > 0)
-				&& (photo.getSmallImage().getHeight() > 0);
-	}
+            // retrieve data from database, so all data will be loaded
+            SpecieDAO sDao = new SpecieDAO();
+            Specie s = sDao.retrieve(photo.getSpecie().getId());
+            photo.setSpecie(s);
 
-	/**
-	 * @param i
-	 * @return
-	 */
-	private static int setSmallWidth(int i) {
-		if (i == 0) {
-			i = W;
-		}
-		return i;
-	}
+            // send email to users
+            logger.debug("Sending email");
+            AbstractPhotoEmailSender emailSender = new PhotoEmailSender(photo);
+            emailSender.sendEmail();
+            
+            if (newSpecie){
+                emailSender = new NewSpecieEmailSender(photo);
+                emailSender.sendEmail();
+                ((NewSpecieEmailSender)emailSender).sendNewSpecieEmail(photo);
+            }
+        } else {
+            throw new ImageProcessingException(
+                    "Real image or thumbnail is wrong" + photo);
+        }
+    }
 
-	/**
-	 * This method creates the thumbnail picture for the given photo object
-	 * 
-	 * @param photo
-	 *            The object with the real image
-	 * 
-	 * @return the path for the temporary file created
-	 * 
-	 * @throws ImageProcessingException
-	 */
-	public static void createSmallPhoto(Photo photo)
-			throws ImageProcessingException {
-		String path = null;
-		ImageFile realimage = photo.getRealImage();
-		ImageFile smallImage = photo.getSmallImage();
-		int size = realimage.getImageSize();
-		if (size == 0) {
-			try {
-				size = realimage.getImage().available();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		}
+    /**
+     * This method verifies if the image was successfully uploaded, and that the
+     * thumbnail was successfully created
+     * 
+     * @param photo
+     *            The photo to be checked
+     * 
+     * @return true if the photo is ok, false otherwise
+     */
+    private boolean isPhotoValid(Photo photo) {
+        return (photo.getRealImage().getWidth() > 0)
+                && (photo.getRealImage().getHeight() > 0)
+                && (photo.getSmallImage().getWidth() > 0)
+                && (photo.getSmallImage().getHeight() > 0);
+    }
 
-		byte[] bytes = new byte[size];
+    /**
+     * @param i
+     * @return
+     */
+    private static int setSmallWidth(int i) {
+        if (i == 0) {
+            i = W;
+        }
+        return i;
+    }
 
-		try {
-			InputStream in = realimage.getImage();
-			in.read(bytes);
-			in.close();
-			ByteArrayInputStream inp = new ByteArrayInputStream(bytes);
-			realimage.setImage(inp);
-		} catch (IOException e) {
-			throw new ImageProcessingException(
-					"Error creating byte[] from InputStream");
-		}
+    /**
+     * This method creates the thumbnail picture for the given photo object
+     * 
+     * @param photo
+     *            The object with the real image
+     * 
+     * @return the path for the temporary file created
+     * 
+     * @throws ImageProcessingException
+     */
+    public static void createSmallPhoto(Photo photo)
+            throws ImageProcessingException {
+        String path = null;
+        ImageFile realimage = photo.getRealImage();
+        ImageFile smallImage = photo.getSmallImage();
+        int size = realimage.getImageSize();
+        if (size == 0) {
+            try {
+                size = realimage.getImage().available();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
 
-		logger.debug("Creating ImageIcon");
-		ImageIcon icon = new ImageIcon(bytes);
-		realimage.setWidth(icon.getIconWidth());
-		realimage.setHeight(icon.getIconHeight());
-		float r = ((float) realimage.getWidth()) / realimage.getHeight();
-		logger.debug("Rate calculated with value = " + r);
-		int hForSmallPhoto = (int) (wForSmallPhoto / r);
-		smallImage.setWidth(wForSmallPhoto);
-		smallImage.setHeight(hForSmallPhoto);
+        byte[] bytes = new byte[size];
 
-		Image image = icon.getImage();
-		path = createFilename();
-		logger.debug("Temporary filename created as " + path);
-		File file = new File(path);
-		RenderedImage thumbnail = null;
-		try {
-			// create the small image
-			thumbnail = createThumbnail(image, wForSmallPhoto, hForSmallPhoto);
-			// write image to temp file
-			ImageIO.write(thumbnail, "jpg", file);
-			try {
-				// create the inputStream from the temp file
-				File newFile = new File(path);
-				InputStream inputStream = new FileInputStream(newFile);
-				smallImage.setImage(inputStream);
-				smallImage.setImageSize((int) newFile.length());
-			} catch (IOException e1) {
-				throw new ImageProcessingException(
-						"Error creating FileInputStream for smallImage...");
-			}
-		} catch (IOException e2) {
-			throw new ImageProcessingException(
-					"Error saving smallImage in a file...");
-		}
+        try {
+            InputStream in = realimage.getImage();
+            in.read(bytes);
+            in.close();
+            ByteArrayInputStream inp = new ByteArrayInputStream(bytes);
+            realimage.setImage(inp);
+        } catch (IOException e) {
+            throw new ImageProcessingException(
+                    "Error creating byte[] from InputStream");
+        }
 
-		logger.debug("Deleting temporary file " + path);
-		File f = new File(path);
-		f.delete();
-	}
+        logger.debug("Creating ImageIcon");
+        ImageIcon icon = new ImageIcon(bytes);
+        realimage.setWidth(icon.getIconWidth());
+        realimage.setHeight(icon.getIconHeight());
+        float r = ((float) realimage.getWidth()) / realimage.getHeight();
+        logger.debug("Rate calculated with value = " + r);
+        int hForSmallPhoto = (int) (wForSmallPhoto / r);
+        smallImage.setWidth(wForSmallPhoto);
+        smallImage.setHeight(hForSmallPhoto);
 
-	/**
-	 * This method creates an thumbnail image for the given image
-	 * 
-	 * @param image
-	 *            The source image, for which a thumbnail will be created
-	 * @param w
-	 *            the new width for the thumbnail
-	 * @param h
-	 *            the new height for the thumbnail
-	 * 
-	 * @return a <code>RenderedImage</code> object for the thumbnail
-	 */
-	private static RenderedImage createThumbnail(Image image, int w, int h) {
-		// Create a buffered image in which to draw
-		BufferedImage bufferedImage = new BufferedImage(w, h,
-				BufferedImage.TYPE_INT_RGB);
+        Image image = icon.getImage();
+        path = createFilename();
+        logger.debug("Temporary filename created as " + path);
+        File file = new File(path);
+        RenderedImage thumbnail = null;
+        try {
+            // create the small image
+            thumbnail = createThumbnail(image, wForSmallPhoto, hForSmallPhoto);
+            // write image to temp file
+            ImageIO.write(thumbnail, "jpg", file);
+            try {
+                // create the inputStream from the temp file
+                File newFile = new File(path);
+                InputStream inputStream = new FileInputStream(newFile);
+                smallImage.setImage(inputStream);
+                smallImage.setImageSize((int) newFile.length());
+            } catch (IOException e1) {
+                throw new ImageProcessingException(
+                        "Error creating FileInputStream for smallImage...");
+            }
+        } catch (IOException e2) {
+            throw new ImageProcessingException(
+                    "Error saving smallImage in a file...");
+        }
 
-		// Create a graphics contents on the buffered image
-		Graphics2D g2d = bufferedImage.createGraphics();
-		g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-				RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-		// Draw graphics
-		g2d.drawImage(image, 0, 0, w, h, null);
+        logger.debug("Deleting temporary file " + path);
+        File f = new File(path);
+        f.delete();
+    }
 
-		// Graphics context no longer needed so dispose it
-		g2d.dispose();
+    /**
+     * This method creates an thumbnail image for the given image
+     * 
+     * @param image
+     *            The source image, for which a thumbnail will be created
+     * @param w
+     *            the new width for the thumbnail
+     * @param h
+     *            the new height for the thumbnail
+     * 
+     * @return a <code>RenderedImage</code> object for the thumbnail
+     */
+    private static RenderedImage createThumbnail(Image image, int w, int h) {
+        // Create a buffered image in which to draw
+        BufferedImage bufferedImage = new BufferedImage(w, h,
+                BufferedImage.TYPE_INT_RGB);
 
-		return bufferedImage;
-	}
+        // Create a graphics contents on the buffered image
+        Graphics2D g2d = bufferedImage.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        // Draw graphics
+        g2d.drawImage(image, 0, 0, w, h, null);
 
-	/**
-	 * @param photo
-	 */
-	public void uploadPhotoForIdentification(Photo photo)
-			throws DatabaseDownException, SQLException,
-			ImageProcessingException {
-		logger
-				.debug("UploadPhoto.uploadPhotoForIdentification : entering method");
-		logger.debug("Calling createSmallPhoto with " + photo);
-		createSmallPhoto(photo);
+        // Graphics context no longer needed so dispose it
+        g2d.dispose();
 
-		if (isPhotoValid(photo)) {
-			PhotoDAO dao = new PhotoDAO();
-			logger.debug("Calling insert in PhotoIdentificationDAO object");
-			dao.insert(photo);
+        return bufferedImage;
+    }
 
-			// retrieve data from database, so all data will be loaded
-			// SpecieDAO sDao = new SpecieDAO();
-			// Specie s = sDao.retrieve(photo.getSpecie().getId());
-			photo.setSpecie(new Specie());
+    /**
+     * @param photo
+     */
+    public void uploadPhotoForIdentification(Photo photo)
+            throws DatabaseDownException, SQLException,
+            ImageProcessingException {
+        logger
+                .debug("UploadPhoto.uploadPhotoForIdentification : entering method");
+        logger.debug("Calling createSmallPhoto with " + photo);
+        createSmallPhoto(photo);
 
-			// send email to users
-			logger.debug("Sending emails for identification...");
-			IdentificationPhotoEmailSender emailSender = new IdentificationPhotoEmailSender(
-					photo);
-			emailSender.sendEmail();
-		} else {
-			throw new ImageProcessingException(
-					"Real image or thumbnail is wrong" + photo);
-		}
-	}
+        if (isPhotoValid(photo)) {
+            PhotoDAO dao = new PhotoDAO();
+            logger.debug("Calling insert in PhotoIdentificationDAO object");
+            dao.insert(photo);
+
+            // retrieve data from database, so all data will be loaded
+            // SpecieDAO sDao = new SpecieDAO();
+            // Specie s = sDao.retrieve(photo.getSpecie().getId());
+            photo.setSpecie(new Specie());
+
+            // send email to users
+            logger.debug("Sending emails for identification...");
+            IdentificationPhotoEmailSender emailSender = new IdentificationPhotoEmailSender(
+                    photo);
+            emailSender.sendEmail();
+        } else {
+            throw new ImageProcessingException(
+                    "Real image or thumbnail is wrong" + photo);
+        }
+    }
 }

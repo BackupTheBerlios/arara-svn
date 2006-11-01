421f80174db23a78e520bad6b7c7bdd7aa46f71f
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2006-11-01 01:28:17 +0000

    Fixed bug: null pointer if user does not chose any value from list

--- core/trunk/src/main/java/net/indrix/arara/servlets/photo/AbstractSearchPhotosServlet.java
+++ core/trunk/src/main/java/net/indrix/arara/servlets/photo/AbstractSearchPhotosServlet.java
@@ -40,30 +40,30 @@ import org.apache.log4j.Logger;
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
 public abstract class AbstractSearchPhotosServlet extends HttpServlet {
-	public static final int PAGINATION_FOR_ALL_PHOTOS = 0;
+    public static final int PAGINATION_FOR_ALL_PHOTOS = 0;
 
-	public static final int PAGINATION_FOR_FAMILY = 1;
+    public static final int PAGINATION_FOR_FAMILY = 1;
 
-	public static final int PAGINATION_FOR_SPECIE = 2;
+    public static final int PAGINATION_FOR_SPECIE = 2;
 
-	public static final int PAGINATION_FOR_COMMON_NAME = 3;
+    public static final int PAGINATION_FOR_COMMON_NAME = 3;
 
-	public static final int PAGINATION_FOR_USER = 4;
+    public static final int PAGINATION_FOR_USER = 4;
 
     public static final int PAGINATION_FOR_RECENT = 5;
 
-	protected static Logger logger = Logger.getLogger("net.indrix.aves");
+    protected static Logger logger = Logger.getLogger("net.indrix.aves");
 
-	protected static Logger loggerActions = Logger
-			.getLogger("net.indrix.actions");
+    protected static Logger loggerActions = Logger
+            .getLogger("net.indrix.actions");
 
-	private static String PHOTOS_BY_PAGE_KEY = "photos.per.page";
+    private static String PHOTOS_BY_PAGE_KEY = "photos.per.page";
 
-	private static int PHOTOS_PER_PAGE;
-	static {
-		PHOTOS_PER_PAGE = Integer.parseInt(PropertiesManager
-				.getProperty(PHOTOS_BY_PAGE_KEY));
-	}
+    private static int PHOTOS_PER_PAGE;
+    static {
+        PHOTOS_PER_PAGE = Integer.parseInt(PropertiesManager
+                .getProperty(PHOTOS_BY_PAGE_KEY));
+    }
 
     /**
      * 
@@ -73,56 +73,69 @@ public abstract class AbstractSearchPhotosServlet extends HttpServlet {
 
         RequestDispatcher dispatcher = null;
         ServletContext context = this.getServletContext();
-        String nextPage = null;
-        List errors = new ArrayList();
+        List <String>errors = new ArrayList<String>();
         HttpSession session = req.getSession();
         User user = (User) session.getAttribute(ServletConstants.USER_KEY);
 
-        String idStr = (String) req.getParameter(ServletConstants.ID);
-        int id = -1;
-        if (idStr != null) {
-            id = Integer.parseInt(idStr);
-        }
-
+        String nextPage = req.getParameter(ServletConstants.NEXT_PAGE_KEY);
+        String pageToShow = req.getParameter(ServletConstants.PAGE_TO_SHOW_KEY);
         String action = req.getParameter(ServletConstants.ACTION);
 
-        List list = null;
-        PhotoPaginationController controller = (PhotoPaginationController) getPaginationController(
-                session, false, getPaginationConstant());
-        controller.setId(id);
-        try {
-            list = controller.doAction(action);
-        } catch (InvalidControllerException e) {
+        String idStr = (String) req.getParameter(ServletConstants.ID);
+        int id = -1;
+        if ((idStr != null) && (idStr.trim().length() > 0)) {
+            id = Integer.parseInt(idStr);
+
+            action = req.getParameter(ServletConstants.ACTION);
+
+            List list = null;
+            PhotoPaginationController controller = (PhotoPaginationController) getPaginationController(
+                    session, false, getPaginationConstant());
+            controller.setId(id);
             try {
-                list = controller.doAction(ServletConstants.BEGIN);
-            } catch (InvalidControllerException e1) {
-                // this should never happend
-                logger.fatal("InvalidControllerException when doing BEGIN action...", e1);
+                list = controller.doAction(action);
+            } catch (InvalidControllerException e) {
+                try {
+                    list = controller.doAction(ServletConstants.BEGIN);
+                } catch (InvalidControllerException e1) {
+                    // this should never happend
+                    logger.fatal("InvalidControllerException when doing BEGIN action...", e1);
+                }
             }
-        }
 
-        logger.debug("List of photos retrieved...");
-        logger.debug("Putting list of photos in session");
-        session.setAttribute(ServletConstants.PHOTOS_LIST, list);
-        session.setAttribute(ServletConstants.SERVLET_TO_CALL_KEY, getServletToCall());
-        nextPage = ServletConstants.ALL_PHOTOS_PAGE;
+            logger.debug("List of photos retrieved...");
+            logger.debug("Putting list of photos in session");
+            session.setAttribute(ServletConstants.PHOTOS_LIST, list);
+            session.setAttribute(ServletConstants.SERVLET_TO_CALL_KEY,
+                    getServletToCall());
+            nextPage = ServletConstants.ALL_PHOTOS_PAGE;
 
-        // adding the user id to request, so it can be sent back by view
-        req.setAttribute(ServletConstants.ID, idStr);
+            // adding the user id to request, so it can be sent back by view
+            req.setAttribute(ServletConstants.ID, idStr);
 
-        if (user != null) {
-            loggerActions.info("User " + user.getLogin() + " from IP " + req.getRemoteAddr() + " has selected photos - " + getPaginationConstant());
+            if (user != null) {
+                loggerActions.info("User " + user.getLogin() + " from IP "
+                        + req.getRemoteAddr() + " has selected photos - "
+                        + getPaginationConstant());
+            } else {
+                String ip = req.getRemoteAddr();
+                Locale locale = req.getLocale();
+                loggerActions.info("Anonymous " + " from IP " + ip + "("
+                        + locale + ") has selected all photos - "
+                        + getPaginationConstant());
+            }
         } else {
-            String ip = req.getRemoteAddr();
-            Locale locale = req.getLocale();
-            loggerActions.info("Anonymous " + " from IP " + ip + "(" + locale + ") has selected all photos - " + getPaginationConstant());
+            errors.add(ServletConstants.SELECT_VALUE_ERROR);
+            
+            req.setAttribute(ServletConstants.NEXT_PAGE_KEY, nextPage);
+            req.setAttribute(ServletConstants.PAGE_TO_SHOW_KEY, pageToShow);
+            req.setAttribute(ServletConstants.ACTION, action);
         }
 
         if (!errors.isEmpty()) {
             logger.debug("errors is not null.");
             // put errors in request
             req.setAttribute(ServletConstants.ERRORS_KEY, errors);
-            nextPage = ServletConstants.UPLOAD_PAGE;
         }
         dispatcher = context.getRequestDispatcher(nextPage);
         logger.debug("Dispatching to " + nextPage);
@@ -130,43 +143,49 @@ public abstract class AbstractSearchPhotosServlet extends HttpServlet {
     }
 
     protected abstract String getServletToCall();
-    
-    protected abstract int getPaginationConstant();
-    
-	protected PaginationController getPaginationController(HttpSession session,
-			boolean identification, int target) {
 
-		// the key is the key string plus the target
-		String key = ServletConstants.PHOTO_PAGINATION_CONTROLLER_KEY + target + identification;
-		logger.debug("AbstractSearchPhotosServlet.getPaginationController: retrieving controller with key " + key);
-		PaginationController c = (PaginationController) session.getAttribute(key);
-		if (c == null) {
-			switch (target) {
-			case PAGINATION_FOR_ALL_PHOTOS:
-				c = new PhotoPaginationController(PHOTOS_PER_PAGE, identification);
-				break;
-			case PAGINATION_FOR_FAMILY:
-				c = new PhotoByFamilyPaginationController(PHOTOS_PER_PAGE);
-				break;
-			case PAGINATION_FOR_SPECIE:
-				c = new PhotoBySpeciePaginationController(PHOTOS_PER_PAGE);
-				break;
-			case PAGINATION_FOR_COMMON_NAME:
-				c = new PhotoByCommonNamePaginationController(PHOTOS_PER_PAGE);
-				break;
-			case PAGINATION_FOR_USER:
-				c = new PhotoByUserPaginationController(PHOTOS_PER_PAGE);
-				break;
+    protected abstract int getPaginationConstant();
+
+    protected PaginationController getPaginationController(HttpSession session,
+            boolean identification, int target) {
+
+        // the key is the key string plus the target
+        String key = ServletConstants.PHOTO_PAGINATION_CONTROLLER_KEY + target
+                + identification;
+        logger
+                .debug("AbstractSearchPhotosServlet.getPaginationController: retrieving controller with key "
+                        + key);
+        PaginationController c = (PaginationController) session
+                .getAttribute(key);
+        if (c == null) {
+            switch (target) {
+            case PAGINATION_FOR_ALL_PHOTOS:
+                c = new PhotoPaginationController(PHOTOS_PER_PAGE,
+                        identification);
+                break;
+            case PAGINATION_FOR_FAMILY:
+                c = new PhotoByFamilyPaginationController(PHOTOS_PER_PAGE);
+                break;
+            case PAGINATION_FOR_SPECIE:
+                c = new PhotoBySpeciePaginationController(PHOTOS_PER_PAGE);
+                break;
+            case PAGINATION_FOR_COMMON_NAME:
+                c = new PhotoByCommonNamePaginationController(PHOTOS_PER_PAGE);
+                break;
+            case PAGINATION_FOR_USER:
+                c = new PhotoByUserPaginationController(PHOTOS_PER_PAGE);
+                break;
             case PAGINATION_FOR_RECENT:
                 c = new PhotoRecentPaginationController(PHOTOS_PER_PAGE);
                 break;
-			}
-			logger.debug("PaginationController just created");
-		} else {
-			logger.debug("PaginationController retrieved from session");
-		}
-		session.setAttribute(key, c);
-		session.setAttribute(ServletConstants.PHOTO_PAGINATION_CONTROLLER_KEY, c);
-		return c;
-	}
+            }
+            logger.debug("PaginationController just created");
+        } else {
+            logger.debug("PaginationController retrieved from session");
+        }
+        session.setAttribute(key, c);
+        session.setAttribute(ServletConstants.PHOTO_PAGINATION_CONTROLLER_KEY,
+                c);
+        return c;
+    }
 }

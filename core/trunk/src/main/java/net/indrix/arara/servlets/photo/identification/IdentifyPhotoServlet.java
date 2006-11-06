6b696b4cf6737e257561223d245e2d8fb7d0c041
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2006-11-06 01:35:07 +0000

    Added support for admin users finish identifications

--- core/trunk/src/main/java/net/indrix/arara/servlets/photo/identification/IdentifyPhotoServlet.java
+++ core/trunk/src/main/java/net/indrix/arara/servlets/photo/identification/IdentifyPhotoServlet.java
@@ -32,174 +32,198 @@ import net.indrix.arara.vo.Specie;
 import net.indrix.arara.vo.User;
 
 /**
- * @author Jeff
+ * This class controls the identification of one photo by a user. The
+ * identification can be a final one in one of two cases:
  * 
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * 1. if the user is the photo owner 2. if the user is an admin and it checked
+ * the checkbox for finishing identification
+ * 
+ * @author Jeff
  */
 public class IdentifyPhotoServlet extends AbstractIdentificationServlet {
-	public void doPost(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
-		logger.info("IdentifyPhotoServlet.doPost : entering method...");
-		List <String>errors = new ArrayList<String>();
-		List <String>messages = new ArrayList<String>();
-		RequestDispatcher dispatcher = null;
-		ServletContext context = this.getServletContext();
-		HttpSession session = req.getSession();
-		String nextPage = null;
 
-		User user = (User) session.getAttribute(ServletConstants.USER_KEY);
-		if (user == null) {
-			logger.debug("errors is not null.");
-			errors.add(ServletConstants.USER_NOT_LOGGED);
-			// put errors in request
-			req.setAttribute(ServletConstants.ERRORS_KEY, errors);
-			nextPage = ServletConstants.LOGIN_PAGE;
-		} else {
-			// retrieve photo under identification
-			Photo photo = (Photo) session.getAttribute(ServletConstants.CURRENT_PHOTO);
-			logger.debug("Photo under identification = " + photo);
+    /**
+     * The constant for telling the page that we're in view mode
+     */
+    private static final String VIEW_MODE = "viewMode";
 
-			String beanKey = IdentificationPhotoConstants.IDENTIFICATION_PHOTO_BEAN;
+    /**
+     * The constant for telling the page that we're in identification mode
+     */
+    private static final String IDENTIFICATION_MODE = "identificationMode";
 
-			IdentifyPhotoBean bean = handleBean(req, session, beanKey, errors);
+    /**
+     * The value of the checkbox for finishing identification when the user
+     * selects it
+     */
+    private static final String ON = "on";
 
-			if (errors.isEmpty()) {
-				logger.debug("IdentifyPhotoServlet.doPost : identification done: " + bean);
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        logger.info("Entering method...");
+        List<String> errors = new ArrayList<String>();
+        List<String> messages = new ArrayList<String>();
+        RequestDispatcher dispatcher = null;
+        ServletContext context = this.getServletContext();
+        HttpSession session = req.getSession();
+        String nextPage = null;
 
-				PhotoIdentification photoIdentification = createObject(bean, user, photo);
-				if (photoIdentification != null) {
-					PhotoModel model = new PhotoModel();
-					try {
+        User user = (User) session.getAttribute(ServletConstants.USER_KEY);
+        if (user == null) {
+            logger.debug("errors is not null.");
+            errors.add(ServletConstants.USER_NOT_LOGGED);
+            // put errors in request
+            req.setAttribute(ServletConstants.ERRORS_KEY, errors);
+            nextPage = ServletConstants.LOGIN_PAGE;
+        } else {
+            boolean finish;
+            if (user.isAdmin()) {
+                String value = req
+                        .getParameter(ServletConstants.FINISH_IDENTIFICATION_KEY);
+                finish = (ON.equals(value)) ? true : false;
+                logger.info("User is admin, retrieve finish identification: "
+                        + finish);
+            } else {
+                finish = false;
+            }
+            // retrieve photo under identification
+            Photo photo = (Photo) session
+                    .getAttribute(ServletConstants.CURRENT_PHOTO);
+            logger.debug("Photo under identification = " + photo);
 
-						if (model.identifyPhoto(photoIdentification)) {
-							req.setAttribute(ServletConstants.IDENTIFICATION_KEY, "false");
-							req.setAttribute(ServletConstants.VIEW_MODE_KEY, "viewMode");
+            String beanKey = IdentificationPhotoConstants.IDENTIFICATION_PHOTO_BEAN;
 
-							messages.add(ServletConstants.END_OF_IDENTIFICATION_MSG);
-							req.setAttribute(ServletConstants.MESSAGES_KEY, messages);
-						} else {
-							// retrieve all identifications already done for the
-							// given photo
-							retrieveIdentificationsForPhoto(model, photo);
-							req.setAttribute(ServletConstants.IDENTIFICATION_KEY, "true");
-							req.setAttribute(ServletConstants.VIEW_MODE_KEY, "identificationMode");
-						}
+            IdentifyPhotoBean bean = handleBean(req, session, beanKey, errors);
 
-						// reset the bean object
-						resetBean(bean);
-					} catch (PhotoAlreadyIdentifiedException e) {
-						logger.debug("PhotoAlreadyIdentifiedException.....", e);
-						errors.add(ServletConstants.PHOTO_ALREADY_IDENTIFIED);
+            if (errors.isEmpty()) {
+                logger.debug("identification done: " + bean);
 
-						// retrieve the photo from database, with updated data
-						try {
-							photo = model.retrieve(photoIdentification
-									.getPhoto().getId());
-							session.setAttribute(
-									ServletConstants.CURRENT_PHOTO, photo);
-						} catch (DatabaseDownException e1) {
-							logger.debug("DatabaseDownException.....");
-							errors.add(ServletConstants.DATABASE_ERROR);
-						} catch (SQLException e1) {
-							logger.debug("SQLException.....", e);
-							errors.add(ServletConstants.DATABASE_ERROR);
-						}
-						req.setAttribute(ServletConstants.IDENTIFICATION_KEY,
-								"false");
-					} catch (DatabaseDownException e) {
-						logger.debug("DatabaseDownException.....");
-						errors.add(ServletConstants.DATABASE_ERROR);
-					} catch (SQLException e) {
-						logger.debug("SQLException.....", e);
-						errors.add(ServletConstants.DATABASE_ERROR);
-					}
-				} else {
-					logger.debug("IdentifyPhotoServlet.doPost : COULD NOT create object...");
-					req.setAttribute(ServletConstants.IDENTIFICATION_KEY, "true");
-				}
-			} else {
-				req.setAttribute(ServletConstants.IDENTIFICATION_KEY, "true");
-				req.setAttribute(ServletConstants.VIEW_MODE_KEY, "identificationMode");
-			}
+                PhotoIdentification photoIdentification = createObject(bean, user, photo);
+                if (photoIdentification != null) {
+                    PhotoModel model = new PhotoModel();
+                    try {
+
+                        if (model.identifyPhoto(photoIdentification, finish)) {
+                            req.setAttribute(ServletConstants.IDENTIFICATION_KEY, "false");
+                            req.setAttribute(ServletConstants.VIEW_MODE_KEY, VIEW_MODE);
+
+                            messages.add(ServletConstants.END_OF_IDENTIFICATION_MSG);
+                            req.setAttribute(ServletConstants.MESSAGES_KEY, messages);
+                        } else {
+                            // retrieve all identifications already done for the
+                            // given photo
+                            retrieveIdentificationsForPhoto(model, photo);
+                            req.setAttribute(ServletConstants.IDENTIFICATION_KEY, "true");
+                            req.setAttribute(ServletConstants.VIEW_MODE_KEY, IDENTIFICATION_MODE);
+                        }
+
+                        // reset the bean object
+                        resetBean(bean);
+                    } catch (PhotoAlreadyIdentifiedException e) {
+                        logger.debug("Exception", e);
+                        errors.add(ServletConstants.PHOTO_ALREADY_IDENTIFIED);
+
+                        // retrieve the photo from database, with updated data
+                        try {
+                            photo = model.retrieve(photoIdentification.getPhoto().getId());
+                            session.setAttribute(ServletConstants.CURRENT_PHOTO, photo);
+                        } catch (DatabaseDownException e1) {
+                            logger.debug("DatabaseDownException.....");
+                            errors.add(ServletConstants.DATABASE_ERROR);
+                        } catch (SQLException e1) {
+                            logger.debug("SQLException.....", e);
+                            errors.add(ServletConstants.DATABASE_ERROR);
+                        }
+                        req.setAttribute(ServletConstants.IDENTIFICATION_KEY, "false");
+                    } catch (DatabaseDownException e) {
+                        logger.debug("DatabaseDownException.....");
+                        errors.add(ServletConstants.DATABASE_ERROR);
+                    } catch (SQLException e) {
+                        logger.debug("SQLException.....", e);
+                        errors.add(ServletConstants.DATABASE_ERROR);
+                    }
+                } else {
+                    logger.debug("IdentifyPhotoServlet.doPost : COULD NOT create object...");
+                    req.setAttribute(ServletConstants.IDENTIFICATION_KEY, "true");
+                }
+            } else {
+                req.setAttribute(ServletConstants.IDENTIFICATION_KEY, "true");
+                req.setAttribute(ServletConstants.VIEW_MODE_KEY, IDENTIFICATION_MODE);
+            }
 
             nextPage = ServletConstants.FRAME_PAGE;
             String pageToShow = "/jsp/photo/search/doShowOnePhoto.jsp";
             req.setAttribute(ServletConstants.PAGE_TO_SHOW_KEY, pageToShow);
 
-			if (!errors.isEmpty()) {
-				// coloca erros no request para registrar.jsp processar e
-				// apresentar mensagem de erro
-				req.setAttribute(ServletConstants.ERRORS_KEY, errors);
-			}
-		}
-		dispatcher = context.getRequestDispatcher(nextPage);
-		dispatcher.forward(req, res);
-	}
+            if (!errors.isEmpty()) {
+                // coloca erros no request para registrar.jsp processar e
+                // apresentar mensagem de erro
+                req.setAttribute(ServletConstants.ERRORS_KEY, errors);
+            }
+        }
+        dispatcher = context.getRequestDispatcher(nextPage);
+        dispatcher.forward(req, res);
+    }
 
-	/**
-	 * This method creates the VO with the identification data
-	 * 
-	 * @param bean
-	 *            The bean with data from user
-	 * @param user
-	 *            The user identifying the photo
-	 * @param photo
-	 *            The photo being identified
-	 * 
-	 * @return A new <code>PhotoIdentification</code> object
-	 */
-	private PhotoIdentification createObject(IdentifyPhotoBean bean, User user,
-			Photo photo) {
-		PhotoIdentification object = new PhotoIdentification();
-		object.setPhoto(photo);
-		object.setUser(user);
+    /**
+     * This method creates the VO with the identification data
+     * 
+     * @param bean
+     *            The bean with data from user
+     * @param user
+     *            The user identifying the photo
+     * @param photo
+     *            The photo being identified
+     * 
+     * @return A new <code>PhotoIdentification</code> object
+     */
+    private PhotoIdentification createObject(IdentifyPhotoBean bean, User user,
+            Photo photo) {
+        PhotoIdentification object = new PhotoIdentification();
+        object.setPhoto(photo);
+        object.setUser(user);
 
-		int ageId = Integer.parseInt(bean.getSelectedAgeId());
-		object.setAge(AgeModel.getAge(ageId));
-		int sexId = Integer.parseInt(bean.getSelectedSexId());
-		object.setSex(SexModel.getSex(sexId));
-		object.setComment(bean.getComment());
+        int ageId = Integer.parseInt(bean.getSelectedAgeId());
+        object.setAge(AgeModel.getAge(ageId));
+        int sexId = Integer.parseInt(bean.getSelectedSexId());
+        object.setSex(SexModel.getSex(sexId));
+        object.setComment(bean.getComment());
 
-		Specie specie = getSpecie(bean.getSelectedSpecieId());
-		object.setSpecie(specie);
-		return object;
-	}
+        Specie specie = getSpecie(bean.getSelectedSpecieId());
+        object.setSpecie(specie);
+        return object;
+    }
 
-	/**
-	 * This method creates the specie object (with default values), and set the
-	 * id into it.
-	 * 
-	 * @param string
-	 *            The id as string
-	 * 
-	 * @return A new Specie object, only with the id set
-	 */
-	private Specie getSpecie(String string) {
-		if (string == null || string.trim().length() == 0) {
-			return null;
-		}
-		int id = Integer.parseInt(string);
+    /**
+     * This method creates the specie object (with default values), and set the
+     * id into it.
+     * 
+     * @param string
+     *            The id as string
+     * 
+     * @return A new Specie object, only with the id set
+     */
+    private Specie getSpecie(String string) {
+        if (string == null || string.trim().length() == 0) {
+            return null;
+        }
+        int id = Integer.parseInt(string);
 
-		SpecieDAO dao = new SpecieDAO();
-		Specie specie = null;
-		try {
-			specie = dao.retrieve(id);
-		} catch (DatabaseDownException e) {
-			logger
-					.fatal("IdentifyPhotoServlet.getSpecie: Could not retrieve Specie object for id "
-							+ id);
-		} catch (SQLException e) {
-			logger
-					.fatal("IdentifyPhotoServlet.getSpecie: Could not retrieve Specie object for id "
-							+ id);
-		} finally {
-			if (specie == null) {
-				specie = new Specie();
-				specie.setId(id);
-			}
-		}
-		return specie;
-	}
+        SpecieDAO dao = new SpecieDAO();
+        Specie specie = null;
+        try {
+            specie = dao.retrieve(id);
+        } catch (DatabaseDownException e) {
+            logger.fatal("Could not retrieve Specie object for id " + id);
+        } catch (SQLException e) {
+            logger.fatal("Could not retrieve Specie object for id " + id);
+        } finally {
+            if (specie == null) {
+                specie = new Specie();
+                specie.setId(id);
+            }
+        }
+        return specie;
+    }
 
 }

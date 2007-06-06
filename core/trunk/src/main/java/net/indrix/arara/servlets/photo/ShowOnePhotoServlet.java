c57f3dadc936f227946bc42c68f5261cae09332a
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2007-06-06 03:38:11 +0000

    Fixed bug in navigation

--- core/trunk/src/main/java/net/indrix/arara/servlets/photo/ShowOnePhotoServlet.java
+++ core/trunk/src/main/java/net/indrix/arara/servlets/photo/ShowOnePhotoServlet.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpSession;
 import net.indrix.arara.dao.DatabaseDownException;
 import net.indrix.arara.model.PhotoModel;
 import net.indrix.arara.servlets.ServletConstants;
-import net.indrix.arara.servlets.pagination.PhotoPaginationController;
+import net.indrix.arara.servlets.pagination.PhotoRecentPaginationController;
 import net.indrix.arara.servlets.pagination.exceptions.InvalidControllerException;
 import net.indrix.arara.vo.Photo;
 
@@ -54,66 +54,54 @@ public class ShowOnePhotoServlet extends AbstractSearchPhotosServlet {
             nextPage = ServletConstants.FRAME_PAGE;
         }
         
+        Photo photo = null;
+        
         List list = null;
         list = (List) session.getAttribute(ServletConstants.PHOTOS_LIST);
-        if (list == null && action == null){
-            // there is no action. Retrieve all photos, order by date
-            list = getList(session, ServletConstants.BEGIN);
-        }
-
-        if ((photoId == null) || (list == null)) {
-            Photo photo = getPhotoFromDatabase(errors, photoId);
-            if (photo != null) {
-                session.setAttribute(ServletConstants.CURRENT_PHOTO, photo);
-            } else {
-                logger.debug("List of photos not found...");
-                nextPage = ServletConstants.ONE_PHOTO_PAGE_ERROR;
+        if (action == null){
+            // there is no action. Retrieve all photos, ordered by date
+            logger.debug("There is no action. Retrieve all photos, ordered by date");
+            try {
+                photo = treatPhotoFromEmailLink(errors, photoId, req, session, ServletConstants.BEGIN);
+                if (photo != null){
+                    session.setAttribute(ServletConstants.CURRENT_PHOTO, photo);
+                } else {
+                    logger.debug("Photo does not exist in DB anymore");
+                    nextPage = ServletConstants.ONE_PHOTO_PAGE_ERROR;                
+                }
+            } catch (DatabaseDownException e) {
+                logger.debug("Photo does not exist in DB anymore");
+                nextPage = ServletConstants.ONE_PHOTO_PAGE_ERROR;                
+            } catch (SQLException e) {
+                logger.debug("Photo does not exist in DB anymore");
+                nextPage = ServletConstants.ONE_PHOTO_PAGE_ERROR;                
             }
         } else {
-            logger.debug("List of photos found...");
-            Iterator it = list.iterator();
-            int id = Integer.parseInt(photoId);
-            boolean found = false;
-            boolean error = false;
-            Photo photo = null;
-            Photo previousPhoto = null;
-            do{
-                int count = 0;
-                while (it.hasNext() && (!found)) {
-                    photo = (Photo) it.next();
-                    count++;
-                    if (photo.getId() == id) {
-                        if (ServletConstants.NEXT.equals(action)){
-                            if (it.hasNext()){
-                                photo = (Photo) it.next();
-                                count++;
-                            } else {
-                                // need to load next page
-                                logger.debug("Reached end of page. Trying next page...");
-                                list = getList(session, action);    
-                                if (list == null){
-                                    photo = getPhotoFromDatabase(errors, photoId);
-                                    if (!errors.isEmpty() || photo == null) {
-                                        logger.debug("Photo does not exist in DB anymore");
-                                        nextPage = ServletConstants.ONE_PHOTO_PAGE_ERROR;
-                                    }
-                                    error = true;
+            logger.debug("Action = " + action);
+            if (list != null){
+                logger.debug("List of photos found...");
+                Iterator it = list.iterator();
+                int id = Integer.parseInt(photoId);
+                boolean found = false;
+                boolean error = false;
+                photo = null;
+                Photo previousPhoto = null;
+                do{
+                    int count = 0;
+                    while (it.hasNext() && (!found)) {
+                        photo = (Photo) it.next();
+                        logger.debug("iteracting over photo " + photo.getId());
+                        count++;
+                        if (photo.getId() == id) {
+                            if (ServletConstants.NEXT.equals(action)){
+                                logger.debug("action = NEXT");
+                                if (it.hasNext()){
+                                    photo = (Photo) it.next();
+                                    logger.debug("Retrieving next photo: " + photo.getId());
+                                    count++;
                                 } else {
-                                    session.setAttribute(ServletConstants.PHOTOS_LIST, list);
-                                    
-                                    it = list.iterator();
-                                    photo = (Photo)it.next();
-                                    count = 1;
-                                }
-                            }
-                        } else {
-                            if (ServletConstants.PREVIOUS.equals(action)){
-                                if (previousPhoto != null){
-                                    photo = previousPhoto;
-                                    count--;
-                                } else {
-                                    // need to load previous page
-                                    logger.debug("Reached begin of page. Trying previous page...");
+                                    // need to load next page
+                                    logger.debug("Reached end of page. Trying next page...");
                                     list = getList(session, action);    
                                     if (list == null){
                                         photo = getPhotoFromDatabase(errors, photoId);
@@ -126,50 +114,77 @@ public class ShowOnePhotoServlet extends AbstractSearchPhotosServlet {
                                         session.setAttribute(ServletConstants.PHOTOS_LIST, list);
                                         
                                         it = list.iterator();
-                                        count = 0;
-                                        while (it.hasNext()){
-                                            count++;
-                                            photo = (Photo)it.next();
-                                        }
-                                    }                                    
+                                        photo = (Photo)it.next();
+                                        count = 1;
+                                    }
+                                }
+                            } else {
+                                if (ServletConstants.PREVIOUS.equals(action)){
+                                    logger.debug("action = PREVIOUS");
+                                    if (previousPhoto != null){
+                                        photo = previousPhoto;
+                                        logger.debug("Retrieving previous photo: " + photo.getId());
+                                        count--;
+                                    } else {
+                                        // need to load previous page
+                                        logger.debug("Reached begin of page. Trying previous page...");
+                                        list = getList(session, action);    
+                                        if (list == null){
+                                            photo = getPhotoFromDatabase(errors, photoId);
+                                            if (!errors.isEmpty() || photo == null) {
+                                                logger.debug("Photo does not exist in DB anymore");
+                                                nextPage = ServletConstants.ONE_PHOTO_PAGE_ERROR;
+                                            }
+                                            error = true;
+                                        } else {
+                                            session.setAttribute(ServletConstants.PHOTOS_LIST, list);
+                                            
+                                            it = list.iterator();
+                                            count = 0;
+                                            while (it.hasNext()){
+                                                count++;
+                                                photo = (Photo)it.next();
+                                            }
+                                        }                                    
+                                    }
                                 }
                             }
+                            logger.debug("Photo found !!!! ");
+                            found = true;
+                            PhotoModel model = new PhotoModel();
+                            try {
+                                // retrieve photo again, since it could be changed by other user.
+                                photo = model.retrieve(photo.getId());
+                                retrieveCommentsForPhoto(model, photo);
+                            } catch (DatabaseDownException e) {
+                                logger.debug("DatabaseDownException.....", e);
+                                errors.add(ServletConstants.DATABASE_ERROR);
+                            } catch (SQLException e) {
+                                logger.debug("SQLException.....", e);
+                                errors.add(ServletConstants.DATABASE_ERROR);
+                            }
                         }
-                        logger.debug("Photo found !!!! ");
-                        found = true;
-                        PhotoModel model = new PhotoModel();
-                        try {
-                            // retrieve photo again, since it could be changed by other user.
-                            photo = model.retrieve(photo.getId());
-                            retrieveCommentsForPhoto(model, photo);
-                        } catch (DatabaseDownException e) {
-                            logger.debug("DatabaseDownException.....", e);
-                            errors.add(ServletConstants.DATABASE_ERROR);
-                        } catch (SQLException e) {
-                            logger.debug("SQLException.....", e);
-                            errors.add(ServletConstants.DATABASE_ERROR);
+                        previousPhoto = photo;
+                    }
+                    if (count > 1 || hasPreviousPage(session, action)){
+                        // there are previous photos
+                        req.setAttribute("hasPrevious", true);
+                    }
+                    if (count < list.size() || hasNextPage(session, action)){
+                        // there are more photos
+                        req.setAttribute("hasNext", true);                
+                    }
+                    if (!found) {
+                        photo = getPhotoFromDatabase(errors, photoId);
+                        if (!errors.isEmpty() || photo == null) {
+                            logger.debug("Photo does not exist in DB anymore");
+                            nextPage = ServletConstants.ONE_PHOTO_PAGE_ERROR;
                         }
+                        error = true;
                     }
-                    previousPhoto = photo;
-                }
-                if (count > 1 || hasPreviousPage(session, action)){
-                    // there are previous photos
-                    req.setAttribute("hasPrevious", true);
-                }
-                if (count < list.size() || hasNextPage(session, action)){
-                    // there are more photos
-                    req.setAttribute("hasNext", true);                
-                }
-                if (!found) {
-                    photo = getPhotoFromDatabase(errors, photoId);
-                    if (!errors.isEmpty() || photo == null) {
-                        logger.debug("Photo does not exist in DB anymore");
-                        nextPage = ServletConstants.ONE_PHOTO_PAGE_ERROR;
-                    }
-                    error = true;
-                }
-            } while (!found && !error);
-            session.setAttribute(ServletConstants.CURRENT_PHOTO, photo);
+                } while (!found && !error);
+                session.setAttribute(ServletConstants.CURRENT_PHOTO, photo);               
+            }
         }
 
         req.setAttribute(ServletConstants.IDENTIFICATION_KEY,identificationStr);
@@ -183,6 +198,68 @@ public class ShowOnePhotoServlet extends AbstractSearchPhotosServlet {
         logger.debug("Dispatching to " + nextPage);
         dispatcher.forward(req, res);
 
+    }
+
+    private Photo treatPhotoFromEmailLink(List <String>errors, String photoId, HttpServletRequest req, 
+            HttpSession session, String begin) throws DatabaseDownException, SQLException {
+        Photo photo = null;
+        PhotoModel model = new PhotoModel();
+        logger.debug("Looking for photo inside database...");
+        photo = getPhotoFromDatabase(errors, photoId);
+        
+        if (photo != null){
+            logger.debug("Retrieving controller...");
+            PhotoRecentPaginationController controller = (PhotoRecentPaginationController) getPaginationController(
+                    session, false, getPaginationConstant(), ServletConstants.BEGIN);
+            List list = null;
+            try {
+                list = controller.doAction(ServletConstants.BEGIN);
+            } catch (InvalidControllerException e) {
+                return photo;
+            }
+            
+            boolean found = false;
+            
+            int id = Integer.parseInt(photoId);
+            int count = 0;
+            Photo tempPhoto = null;
+            
+            logger.debug("Iterating over list, with size = " + list.size());
+            while (!found){
+                Iterator it = list.iterator();
+
+                while (it.hasNext() && !found){
+                    tempPhoto = (Photo)it.next();
+                    count++;
+                    if (tempPhoto.getId() == id){
+                        logger.debug("Photo found inside list...");
+                        // photo found.
+                        if (count > 1 || hasPreviousPage(session, ServletConstants.PREVIOUS)){
+                            // there are previous photos
+                            logger.debug("Setting hasPrevious into request...");
+                            req.setAttribute("hasPrevious", true);
+                        }
+                        if (count < list.size() || hasNextPage(session, ServletConstants.NEXT)){
+                            // there are more photos
+                            logger.debug("Setting hasNext into request...");
+                            req.setAttribute("hasNext", true);                
+                        }
+                        
+                        session.setAttribute(ServletConstants.PHOTOS_LIST, list);
+                        found = true;
+                    }
+                }
+                if (!found){
+                    try {
+                        list = controller.doAction(ServletConstants.NEXT);
+                    } catch (InvalidControllerException e) {
+                        return photo;
+                    }
+                }
+            }
+        }
+        return photo;
+        
     }
 
     /**
@@ -247,14 +324,14 @@ public class ShowOnePhotoServlet extends AbstractSearchPhotosServlet {
 
     private boolean hasNextPage(HttpSession session, String action) {
         logger.debug("Retrieving controller...");
-        PhotoPaginationController controller = (PhotoPaginationController) getPaginationController(
+        PhotoRecentPaginationController controller = (PhotoRecentPaginationController) getPaginationController(
                 session, false, getPaginationConstant(), action);
         return controller.hasNext();
     }
 
     private boolean hasPreviousPage(HttpSession session, String action) {
         logger.debug("Retrieving controller...");
-        PhotoPaginationController controller = (PhotoPaginationController) getPaginationController(
+        PhotoRecentPaginationController controller = (PhotoRecentPaginationController) getPaginationController(
                 session, false, getPaginationConstant(), action);
         return controller.hasPrevious();
     }
@@ -263,7 +340,7 @@ public class ShowOnePhotoServlet extends AbstractSearchPhotosServlet {
     private List getList(HttpSession session, String action){
         List list = null;
         logger.debug("Retrieving controller...");
-        PhotoPaginationController controller = (PhotoPaginationController) getPaginationController(
+        PhotoRecentPaginationController controller = (PhotoRecentPaginationController) getPaginationController(
                 session, false, getPaginationConstant(), action);
         try {
             logger.debug("Controller retrieved... Calling doAction...");
@@ -283,7 +360,7 @@ public class ShowOnePhotoServlet extends AbstractSearchPhotosServlet {
 
     @Override
     protected int getPaginationConstant() {
-        return PAGINATION_FOR_ALL_PHOTOS;
+        return PAGINATION_FOR_RECENT;
     }
 
     @Override

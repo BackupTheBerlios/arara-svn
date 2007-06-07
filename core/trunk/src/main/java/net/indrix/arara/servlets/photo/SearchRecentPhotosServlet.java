b49c25911439eac7e6b878552abe8476451a44d0
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2007-06-07 14:12:21 +0000

    added @SuppressWarnings("serial")

--- core/trunk/src/main/java/net/indrix/arara/servlets/photo/SearchRecentPhotosServlet.java
+++ core/trunk/src/main/java/net/indrix/arara/servlets/photo/SearchRecentPhotosServlet.java
@@ -1,5 +1,6 @@
 package net.indrix.arara.servlets.photo;
 
+@SuppressWarnings("serial")
 public class SearchRecentPhotosServlet extends SearchPhotosServlet {
     protected int getPaginationConstant() {
         return PAGINATION_FOR_RECENT;

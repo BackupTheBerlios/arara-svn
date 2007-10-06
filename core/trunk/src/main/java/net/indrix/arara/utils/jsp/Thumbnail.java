18f7ba985fe645d53cec17650125ff4631d6f294
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2007-10-06 23:43:35 +0000

    Fixed issue with getScaledHeight and getHeight

--- core/trunk/src/main/java/net/indrix/arara/utils/jsp/Thumbnail.java
+++ core/trunk/src/main/java/net/indrix/arara/utils/jsp/Thumbnail.java
@@ -13,7 +13,45 @@ package net.indrix.arara.utils.jsp;
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
 public class Thumbnail {
+    static int MAX = 5;
+    /**
+     * This method retrieves the scaled height for the given width
+     * 
+     * @param thumbnailTargetWidth
+     *            The target width
+     * @param imageWidth
+     *            The photo width
+     * @param imageHeight
+     *            The photo heigth
+     * 
+     * @return The new scaled height of the photo
+     */
+    public static int getScaledWidth(int thumbnailTargetWidth, int imageWidth,
+            int imageHeight) {
+        int w = 0, h = 0, hForSmallPhoto = 0;
+        float rate;
+        float rate1;
+        boolean cool = false;
+        float smallerDifference = 1;
+        for (int i = thumbnailTargetWidth - MAX; (!cool) && (i < thumbnailTargetWidth + MAX); i++){
+            rate = ((float) imageWidth) / imageHeight;       
+            float f = (i / (float)rate);
+            hForSmallPhoto = (int) Math.round(f);
 
+            rate1 = ((float) i) / hForSmallPhoto;
+            
+            
+            if (smallerDifference > Math.abs(rate - rate1)){
+                smallerDifference = Math.abs(rate - rate1);
+                w = i;
+                h = hForSmallPhoto;
+            }
+                
+        }
+        
+        return w;
+    }
+    
 	/**
 	 * This method retrieves the scaled height for the given width
 	 * 
@@ -28,12 +66,28 @@ public class Thumbnail {
 	 */
 	public static int getScaledHeight(int thumbnailTargetWidth, int imageWidth,
 			int imageHeight) {
+        int w, h = 0, hForSmallPhoto = 0;
+        float rate;
+        float rate1;
+        boolean cool = false;
+        float smallerDifference = 1;
+        for (int i = thumbnailTargetWidth - MAX; (!cool) && (i < thumbnailTargetWidth + MAX); i++){
+            rate = ((float) imageWidth) / imageHeight;       
+            float f = (i / (float)rate);
+            hForSmallPhoto = (int) Math.round(f);
 
-		float rate = ((float) imageWidth) / imageHeight;       
-        float f = (thumbnailTargetWidth / (float)rate);
-        int hForSmallPhoto = (int) Math.round(f);
-
-		return hForSmallPhoto;
+            rate1 = ((float) i) / hForSmallPhoto;
+            
+            
+            if (smallerDifference > Math.abs(rate - rate1)){
+                smallerDifference = Math.abs(rate - rate1);
+                w = i;
+                h = hForSmallPhoto;
+            }
+                
+        }
+        
+		return h;
 	}
 
 	/**
@@ -89,7 +143,12 @@ public class Thumbnail {
 	}
     
     public static void main(String args[]){
-        int h = getHeight(600, 800, 533);
+        int s = 600;
+        int w = getScaledWidth(s, 800, 533);
+        int h = getScaledHeight(s, 800, 533);
+        System.out.println(w);
         System.out.println(h);
+        System.out.println((float)w/h);
+        System.out.println((float)800/533);
     }
 }

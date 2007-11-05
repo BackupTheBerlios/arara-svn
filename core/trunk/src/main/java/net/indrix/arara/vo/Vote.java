5bfa1b052b8bae3bdd4b0472921a0dbbeff0b82a
jefferson <jefferson@3511fa1d-520a-0410-8d26-dc3463b4c29b>
2007-11-05 21:50:00 +0000

    Initial version

---
+++ core/trunk/src/main/java/net/indrix/arara/vo/Vote.java
@@ -0,0 +1,57 @@
+package net.indrix.arara.vo;
+
+import java.util.Date;
+
+public class Vote {
+    private int id;
+
+    private Photo photo;
+
+    private User user;
+
+    private int vote;
+
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Photo getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Photo photo) {
+        this.photo = photo;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getVote() {
+        return vote;
+    }
+
+    public void setVote(int vote) {
+        this.vote = vote;
+    }
+    
+    
+}
